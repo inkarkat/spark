975e4f7198389c30eda525fca8ba166ad5031134
Ingo Karkat <swdev@ingo-karkat.de>
2021-06-30 21:34:35 +0200

    Tests: Style: Inline $data, remove empty lines, consistently quote $graph

--- tests/spark-test.sh
+++ tests/spark-test.sh
@@ -20,87 +20,69 @@ it_shows_help_with_help_option() {
 it_graphs_argv_data() {
   graph="$($spark 1,5,22,13,5)"
 
-  test $graph = '▁▂█▅▂'
+  test "$graph" = '▁▂█▅▂'
 }
 
 it_charts_pipe_data() {
-  data="0,30,55,80,33,150"
-  graph="$(echo $data | $spark)"
+  graph="$(echo 0,30,55,80,33,150 | $spark)"
 
-  test $graph = '▁▂▃▄▂█'
+  test "$graph" = '▁▂▃▄▂█'
 }
 
 it_charts_spaced_data() {
-  data="0 30 55 80 33 150"
-  graph="$($spark $data)"
+  graph="$($spark 0 30 55 80 33 150)"
 
-  test $graph = '▁▂▃▄▂█'
+  test "$graph" = '▁▂▃▄▂█'
 }
 
 it_charts_way_spaced_data() {
-  data="0 30               55 80 33     150"
-  graph="$($spark $data)"
+  graph="$($spark 0 30               55 80 33     150)"
 
-  test $graph = '▁▂▃▄▂█'
+  test "$graph" = '▁▂▃▄▂█'
 }
 
 it_handles_decimals() {
-  data="5.5,20"
-  graph="$($spark $data)"
-
-  test $graph = '▁█'
+  graph="$($spark 5.5,20)"
+  test "$graph" = '▁█'
 }
 
 it_charts_100_lt_300() {
-  data="1,2,3,4,100,5,10,20,50,300"
-  graph="$($spark $data)"
-
-  test $graph = '▁▁▁▁▃▁▁▁▂█'
+  graph="$($spark 1,2,3,4,100,5,10,20,50,300)"
+  test "$graph" = '▁▁▁▁▃▁▁▁▂█'
 }
 
 it_charts_50_lt_100() {
-  data="1,50,100"
-  graph="$($spark $data)"
-
-  test $graph = '▁▄█'
+  graph="$($spark 1,50,100)"
+  test "$graph" = '▁▄█'
 }
 
 it_charts_4_lt_8() {
-  data="2,4,8"
-  graph="$($spark $data)"
-
-  test $graph = '▁▃█'
+  graph="$($spark 2,4,8)"
+  test "$graph" = '▁▃█'
 }
 
 it_charts_no_tier_0() {
-  data="1,2,3,4,5"
-  graph="$($spark $data)"
-
-  test $graph = '▁▂▄▆█'
+  graph="$($spark 1,2,3,4,5)"
+  test "$graph" = '▁▂▄▆█'
 }
 
 it_equalizes_at_midtier_on_same_data() {
-  data="1,1,1,1"
-  graph="$($spark $data)"
-
-  test $graph = '▅▅▅▅'
+  graph="$($spark 1,1,1,1)"
+  test "$graph" = '▅▅▅▅'
 }
 
 it_renders_x_as_unknown_data() {
   graph="$($spark 0 30 55 80 x x 33 x 150)"
-
   test "$graph" = '▁▂▃▄××▂×█'
 }
 
 it_keeps_space_argument() {
   graph="$($spark 0 30 55 80 ' ' ' ' 33 ' ' 150)"
-
   test "$graph" = '▁▂▃▄  ▂ █'
 }
 
 it_keeps_newline_argument() {
   graph="$($spark 0 30 55 80 $'\n' $'\n' 33 $'\n' 150)"
-
   test "$graph" = '▁▂▃▄
 
 ▂
@@ -109,64 +91,50 @@ it_keeps_newline_argument() {
 
 it_keeps_space_argument() {
   graph="$($spark 0 30 55 80 ' ' ' ' 33 ' ' 150)"
-
   test "$graph" = '▁▂▃▄  ▂ █'
 }
 
 it_uses_auto_min_max() {
   graph="$($spark 25 30 45 50 40 35 30 25 50)"
-
   test "$graph" = '▁▂▆█▅▃▂▁█'
 }
 
 it_uses_fixed_min() {
-
   graph="$($spark --min 0 -- 25 30 45 50 40 35 30 25 50)"
-
   test "$graph" = '▄▅▇█▆▅▅▄█'
 }
 
 it_uses_fixed_max() {
-
   graph="$($spark --max 100 -- 25 30 45 50 40 35 30 25 50)"
-
   test "$graph" = '▁▁▂▃▂▁▁▁▃'
 }
 
 it_uses_fixed_min_max() {
-
   graph="$($spark --min 0 --max 100 -- 25 30 45 50 40 35 30 25 50)"
-
   test "$graph" = '▂▃▄▄▃▃▃▂▄'
 }
 
 it_uses_special_symbol_for_below_fixed_min() {
-
   graph="$($spark --min 25 -- 24 25 30 45 50 40 35 30 25 0 50)"
-
   test "$graph" = '⭳▁▂▆█▅▃▂▁⭳█'
 }
 
 it_uses_special_symbol_for_above_fixed_max() {
   graph="$($spark --max 50 -- 25 30 45 50 100 40 35 30 25 50 51)"
-
   test "$graph" = '▁▂▆█⭱▅▃▂▁█⭱'
 }
 
 it_scales_common_logarithmically() {
   graph="$($spark --scale log10 -- 1 10 100 1000 10000 100000)"
-
   test "$graph" = '▁▂▃▅▆█'
 }
 
 it_scales_natural_logarithmically() {
   graph="$($spark --scale ln -- 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192)"
-
   test "$graph" = '▁▁▂▂▃▄▄▄▅▅▆▇█'
 }
 
 it_scales_binary_logarithmically() {
   graph="$($spark --scale ld -- 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192)"
-
   test "$graph" = '▁▁▂▂▃▃▄▅▅▆▆▇█'
 }
